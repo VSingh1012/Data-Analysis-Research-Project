56f0ad3b310025cab5872396d69835c28bfd9dbe
VSingh1012 <72770416+VSingh1012@users.noreply.github.com>
2024-06-13 23:28:00 -0400

    Update Selection.py

--- Selection.py
+++ Selection.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import filedialog
+from tkinter import PhotoImage
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
@@ -11,90 +12,98 @@ import customtkinter as ctk
 from PIL import ImageTk, Image
 
 
+class SelectionScreen:
 
-class SelectionScreen: 
-  
+  def __init__(self, win):
+    self.win = win
 
-    def __init__(self, win):
-      self.win = win
-      
+  def __call__(self):
 
-  
-    def __call__(self):
+    icon_image = PhotoImage(file="DatafyLogo.png")
 
-      ctk.set_appearance_mode("dark")
-      
-      self.win.withdraw()
-      
-      main_screen = ctk.CTkToplevel(self.win)
+    ctk.set_appearance_mode("dark")
 
-      main_screen.title("Data Analyzer")
-      
-      main_screen.geometry("400x400")
+    self.win.withdraw()
 
-      main_screen.resizable(True, True)
+    main_screen = ctk.CTkToplevel(self.win)
 
-  
-      
-      csv_file = filedialog.askopenfilename()
+    main_screen.title("Data Analyzer")
 
-      dataframe = pd.read_csv(csv_file)
+    main_screen.geometry("400x400")
 
-      fileLabel = ctk.CTkLabel(master=self.win, text=" ", bg_color='white', font=("Comic Sans", 10), text_color='black')
-      fileLabel.pack()
+    main_screen.resizable(True, True)
 
-      fileLabel.configure(text="File used: " + csv_file)
+    main_screen.iconphoto(False, icon_image)
 
-      bar = Bar(self.win, dataframe)
+    csv_file = filedialog.askopenfilename()
 
-      scatter = Scatter(self.win, dataframe)
+    dataframe = pd.read_csv(csv_file)
 
-      linreg = LinReg(self.win, dataframe)
+    fileLabel = ctk.CTkLabel(master=self.win,
+                             text=" ",
+                             bg_color='white',
+                             font=("Comic Sans", 10),
+                             text_color='black')
+    fileLabel.pack()
 
+    fileLabel.configure(text="File used: " + csv_file)
 
-      def back_to_main():
-        self.win.deiconify()
+    bar = Bar(self.win, dataframe)
 
-        main_screen.withdraw()
-      
+    scatter = Scatter(self.win, dataframe)
 
-      
-      selection_title = ctk.CTkLabel(master=main_screen, text="Data Analyzer", font=('Comic Sans Bold', 35), text_color="#399adb")
+    linreg = LinReg(self.win, dataframe)
 
-      selection_title.pack()
+    def back_to_main():
+      self.win.deiconify()
 
-      bar_image = ctk.CTkImage(Image.open("bargraph.png"), size=(60, 60))
+      main_screen.withdraw()
 
-      scatter_image = ctk.CTkImage(Image.open("scatterplot.png"), size=(60, 60))
+    selection_title = ctk.CTkLabel(master=main_screen,
+                                   text="Data Analyzer",
+                                   font=('Comic Sans Bold', 35),
+                                   text_color="#399adb")
 
-      linreg_image = ctk.CTkImage(Image.open("linreg.png"), size=(60, 60))
+    selection_title.pack()
 
-      # bar_img_label = ctk.CTkLabel(master=main_screen, , text=" ")
+    bar_image = ctk.CTkImage(Image.open("bargraph.png"), size=(60, 60))
 
-      # bar_img_label.pack(padx=10, pady= 2)
-      bar_button = ctk.CTkButton(master=main_screen, image=bar_image, text="Go to Bar Plot page", font= ("Arial", 15), fg_color="#399adb", command=bar.__call__)
+    scatter_image = ctk.CTkImage(Image.open("scatterplot.png"), size=(60, 60))
 
-      bar_button.pack(padx=10, pady=15)
+    linreg_image = ctk.CTkImage(Image.open("linreg.png"), size=(60, 60))
 
-      scatter_button = ctk.CTkButton(main_screen, image=scatter_image, text="Go to Scatter Plot page", font= ('Arial', 15), fg_color="#399adb", command=scatter.__call__)
+    bar_button = ctk.CTkButton(master=main_screen,
+                               image=bar_image,
+                               text="Go to Bar Plot page",
+                               font=("Arial", 15),
+                               fg_color="#399adb",
+                               command=bar.__call__)
 
-      scatter_button.pack(padx= 10, pady = 20)
+    bar_button.pack(padx=10, pady=15)
 
-      linreg_button = ctk.CTkButton(main_screen, image=linreg_image, text="Go to Linear Regression Graph page", font= ('Arial', 15), fg_color="#399adb", command=linreg.__call__)
+    scatter_button = ctk.CTkButton(main_screen,
+                                   image=scatter_image,
+                                   text="Go to Scatter Plot page",
+                                   font=('Arial', 15),
+                                   fg_color="#399adb",
+                                   command=scatter.__call__)
 
-      linreg_button.pack(padx = 10, pady = 30)
+    scatter_button.pack(padx=10, pady=20)
 
+    linreg_button = ctk.CTkButton(main_screen,
+                                  image=linreg_image,
+                                  text="Go to Linear Regression Graph page",
+                                  font=('Arial', 15),
+                                  fg_color="#399adb",
+                                  command=linreg.__call__)
 
-      back_button = ctk.CTkButton(master=main_screen, text="Back to Main Menu", fg_color="#399adb", height=25, font=("Arial", 15), command=back_to_main)
+    linreg_button.pack(padx=10, pady=30)
 
-      back_button.pack(padx=10, pady=34)
+    back_button = ctk.CTkButton(master=main_screen,
+                                text="Back to Main Menu",
+                                fg_color="#399adb",
+                                height=25,
+                                font=("Arial", 15),
+                                command=back_to_main)
 
-
-    
-
-
-      
-      
-
-      
-    
+    back_button.pack(padx=10, pady=34)
